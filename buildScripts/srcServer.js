2e96215e389d588afb868b9d2e577c297b41e358
rabobahago <rabobahago@gmail.com>
2021-06-17 11:02:28 +0100

    added webpack and configured webpack for bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,8 +1,16 @@
 import express from "express";
 import path from "path";
 import open from "open";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
+const compiler = webpack(config);
 const app = express();
 const port = 3000;
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    publicPath: config.output.publicPath,
+  })
+);
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../src/index.html"));
 });
