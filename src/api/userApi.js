ff197aa2c496c7369c1b7fd8955e4f5b0c9a5946
rabobahago <rabobahago@gmail.com>
2021-06-18 09:08:18 +0100

    created mock data with fake api

---
+++ src/api/userApi.js
@@ -0,0 +1,25 @@
+import "whatwg-fetch";
+import getBaseUrl from "./baseUrl";
+const baseUrl = getBaseUrl();
+
+export function getUsers() {
+  return get("users");
+}
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+function get(url) {
+  return fetch(baseUrl + url).then(onSuccess, onError);
+}
+function del(url) {
+  const reuest = new Request(baseUrl + url, {
+    method: "DELETE",
+  });
+  return fetch(reuest).then(onSuccess, onError);
+}
+function onSuccess(response) {
+  return response.json();
+}
+function onError(error) {
+  console.log(error); // eslint-disable-line no-console
+}
--- src/index.test.js
+++ src/index.test.js
@@ -9,12 +9,12 @@ describe("our first test", () => {
 });
 
 describe("index.html", () => {
-  it("should say hello world!", () => {
+  it("should have h1 that users", () => {
     const index = fs.readFileSync("./src/index.html", "utf-8");
     const { JSDOM } = jsdom;
     const dom = new JSDOM(index);
-    const h1 = dom.window.document.getElementsByTagName("h2")[0];
-    expect(h1.innerHTML).to.equal("Hello World!");
+    const h1 = dom.window.document.getElementsByTagName("h1")[0];
+    expect(h1.innerHTML).to.equal("Users");
     dom.window.close();
   });
 });
